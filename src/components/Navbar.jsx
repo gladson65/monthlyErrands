3154399b0b85a31138a6a4ef6a629418ad212ffd
gladson65 <grauth65@gmail.com>
2026-02-04 16:46:56 +0530

    tailwindcss imported, navbar header and nav section added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ function Navbar() {
     return (
 
         <>
-        
+            <header className="w-dvw h-20 bg-red-400">
+                <nav>
+                    
+                </nav>
+            </header>
         </>
 
     )
