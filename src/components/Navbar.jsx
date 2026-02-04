d6977e29bf467e2c588fab8a0fb47c3ed1107dbc
gladson65 <grauth65@gmail.com>
2026-02-04 16:58:08 +0530

    Link is added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom'
 
 function Navbar() {
 
     return (
 
         <>
-            <header className="w-dvw h-20 bg-red-400">
-                <nav>
-                    
+            <header className="w-dvw h-20 bg-red-400 p-4">
+                <nav className="w-full h-full bg-red-500">
+                    <div id="logo">
+                        <h1>Monthly Errands</h1>
+                    </div>
+                    <div id="navigation">
+                        <Link to={"register"}>Sign Up</Link>
+                    </div>
                 </nav>
             </header>
         </>
